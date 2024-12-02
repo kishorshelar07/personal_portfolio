3ce66b04ea846d9ce8ff55f616509506fae70c97
kishor <kishorshelar560@gmail.com>
2024-12-02 12:13:38 +0530

    This repository serves as my personal portfolio, highlighting my skills in web development, UI/UX design, and full-stack applications. Explore my work, including projects and demos, to see how I bring ideas to life with clean code and creative design.

---
+++ src/content_option.js
@@ -0,0 +1,193 @@
+const logotext = "Kishor S";
+const meta = {
+    title: "Kishor Shelar",
+    description: "I’m Kishor Shelar Full stack devloper,currently working in Pune.",
+};
+
+const introdata = {
+    title: "I’m Kishor Shelar",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I develop mobile apps",
+    },
+    description: "An ambitious professional with a knack for creativity and resilience, seeking to excel in a challenging and innovative role.",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "I am a 27-year-old creative and resilient individual, passionate about innovation and personal growth, with a strong focus on achieving excellence in every endeavor.",
+};
+const worktimeline = [{
+        jobtitle: "Full Stack Web Developer ",
+        where: "C-infotech Pune",
+        date: " July 2023",
+    }
+];
+
+const skills = [{
+        name: "HTML",
+        value: 90,
+    },
+    {
+        name: "CSS",
+        value: 80,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "Jquery",
+        value: 60,
+    },
+    {
+        name: "Bootstrap 5",
+        value: 70,
+    },
+    {
+        name: "PHP",
+        value: 65,
+    },
+    {
+        name: "MySQL",
+        value: 70,
+    },
+    {
+        name: "React JS",
+        value: 70,
+    },
+    {
+        name: "Node Js/Express js",
+        value: 60,
+    },
+    {
+        name: "Mongo DB",
+        value: 65,
+    },
+    {
+        name: "API's",
+        value: 70,
+    },
+    {
+        name: "AJAX",
+        value: 60,
+    }
+
+    
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "I am a passionate UI/UX designer focused on crafting intuitive and visually appealing digital experiences. With a keen eye for detail and a user-centered approach, I specialize in designing interfaces that seamlessly blend functionality with aesthetics, ensuring exceptional user satisfaction.",
+    },
+    {
+        title: "Full-Stack Development",
+        description: "As a full-stack developer, I build responsive and dynamic web applications, combining efficient back-end logic with interactive front-end designs to deliver high-performance solutions.",
+    },
+    {
+        title: "Front-End Development",
+        description: "I am a front-end developer passionate about turning ideas into visually appealing and interactive websites using modern technologies like React, HTML, CSS, and JavaScript.",
+    },
+    {
+        title: "Back-End Development",
+        description: "I am a front-end developer passionate about turning ideas into visually appealing and interactive websites using modern technologies like React, HTML, CSS, and JavaScript.",
+    },
+    {
+        title: "Custom Website Development",
+        description: "I create tailor-made websites designed to meet specific client requirements, integrating unique features and personalized designs to stand out.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "kishorshelar560@gmail.com",
+    YOUR_FONE: "+91 8308516345",
+    description: "Have a project in mind or want to collaborate? Feel free to reach out through the form below or contact me directly—I’d love to connect! ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/kishorshelar07",
+    linkedin: "https://www.linkedin.com/in/kishorshelar1610/",
+    twitter: "https://x.com/kishorshelar560",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
